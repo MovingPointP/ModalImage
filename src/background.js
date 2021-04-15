591b260f87f42586e1c2a12d46876515ec0175de
MovingPointP <kobayashi.akio39@gmail.com>
2021-04-15 20:54:14 +0900

    [UPDATE]いらないファイルの削除やコメントの追加

--- src/background.js
+++ src/background.js
@@ -1,4 +1,5 @@
-chrome.runtime.onInstalled.addListener( () => {
+//インストール後に実行
+chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set(
         {'switch': true }
     )
@@ -14,12 +15,23 @@ chrome.runtime.onInstalled.addListener( () => {
         parentId: parent_menu,
         title: 'モーダルを表示しないサイトに設定'
     })
+
+    chrome.contextMenus.create({
+        id: 'option-page',
+        parentId: parent_menu,
+        title: 'optionページへ'
+    })
 })
 
+//menuを押して実行
 chrome.contextMenus.onClicked.addListener((item) => {
     if (item.menuItemId == 'set-url') {
         chrome.tabs.executeScript({
             file: "/seturl.js"
         })
+    }
+
+    if (item.menuItemId == 'option-page') {
+        chrome.tabs.create({ 'url': '/options.html' })
     }
 })
--- src/seturl.js
+++ src/seturl.js
@@ -1,6 +1,8 @@
 const protocol = location.protocol
 const host = location.host
 var url = protocol + host
+
+//url(protocolとhost名)を追加
 chrome.storage.sync.get(['urlList'], (items) => {
     var blackList = items.urlList
     if (!blackList) {
